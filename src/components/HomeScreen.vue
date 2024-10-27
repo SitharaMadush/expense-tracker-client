46eed40620a15225b3e2846bab656b415300d0e7
Sithara <sitharamadushankp@gmail.com>
2024-10-27 08:55:53 +0530

    Project set-up with vue 3, pinia, vue router and tailwind CSS

---
+++ src/components/HomeScreen.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <h1 class="text-orange-400	">Home Page</h1>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import LoginScreen from '../components/auth/LoginScreen.vue'
+</script>
+
+<template>
+  <main>
+    <LoginScreen />
+  </main>
+</template>
